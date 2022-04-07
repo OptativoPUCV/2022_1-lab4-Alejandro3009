41aa6689084bf04471c3d020db7e87f003f3b126
Your Name <you@example.com>
2022-04-07 12:51:50 -0300

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,11 +53,10 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity) {
   HashMap *map = (HashMap *) malloc (sizeof(HashMap));
+  map->buckets = (Pair**) calloc (capacity,sizeof(Pair *));
   map->capacity = capacity;
   map->size = 0;
   map->current = -1;
-
-  //map->buckets = (Pair**) malloc (sizeof(Pair*));
   return map;
 }
 
